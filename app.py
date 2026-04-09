d08a3b37b0a6ef381d8eddf439f0439eb067eab1
vijay dholpuria <vijay635037@gmail.com>
2026-04-09 12:19:58 +0530

    fix port

--- app.py
+++ app.py
@@ -931,4 +931,5 @@ def contact():
     return render_template("contact.html")
 # ---------------- RUN ----------------
 
-app.run(host="0.0.0.0", port=5000, debug=False)
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
